904f12e082d30682e6d98d535555b8e6068aaef3
Matthew Cox <mattdrummerguy@gmail.com>
2023-01-31 15:12:01 -0600

    This is how we add and commit files!

---
+++ test.js
@@ -0,0 +1,4 @@
+//console.log is a way to display code in your browser's console section.
+//Download live server by Ritwick Dey to view it in your browser.
+//This is vsc code and is the section where you will edit code.
+console.log("This is some sample text that will show in my browser.");
